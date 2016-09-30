71c0fbea4f78c13d1059626bbabd50973dfa9d8f
SeregA <F:\ \The Bat!>
2016-09-30 16:18:38 +0300

    feature/main_screen done

--- example/src/main/java/com/github/paolorotolo/appintroexample/MainActivity.java
+++ example/src/main/java/com/github/paolorotolo/appintroexample/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import com.github.paolorotolo.appintroexample.animations.ColorAnimation;
 import com.github.paolorotolo.appintroexample.animations.CustomAnimation;
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
     }
 
     public void startDefaultIntro(View v) {
